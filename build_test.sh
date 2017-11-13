751b2f0ef87d711fd13b5ede69c8cceb2d00d292
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-11-12 19:23:01 -0800

    adding in lychee and fujian

--- build_test.sh
+++ build_test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #
 # Copyright 2016 Christopher Antila
+# Copyright 2017 Jeff Treviño
 
-echo "Running all the test suites"
+echo "Running all the test suites."
 # wd: macOS/
-
+echo "Running Julius tests."
 cd julius && npm test && cd ..
-# cd fujian && py.test && cd ..
-# cd lychee && py.test && cd ..
+cd fujian && py.test && cd ..
+cd lychee && py.test && cd ..
+echo "Running mercurial tests."
 cd mercurial-hug && py.test && cd ..
