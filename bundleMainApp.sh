2ea146b6888f6ba95bc59f24e19ce70cf221bf74
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2018-01-14 19:15:27 -0800

    removed editable mode from production requirements

--- bundleMainApp.sh
+++ bundleMainApp.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+    #!/usr/bin/env bash
 #
 # Copyright 2016 Jeff Trevino
 
@@ -50,6 +50,7 @@ if ! [ -d julius ]
 then
     git clone --recursive https://github.com/nCoda/julius.git julius
     cd julius # -> macOS/build/julius
+    git checkout cb586c43dd55f7055bd38182f4800628e19e64b4
     npm install && cd .. # -> macOS/build/
 fi
 # build Electron app
